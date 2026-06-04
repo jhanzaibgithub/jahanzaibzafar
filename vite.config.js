e90bc906c5a71ccf879875ec6ebd5025ad94f5c4
jhanzaibgithub <jahanzaibryk2020@gmail.com>
2026-06-04 17:29:18 +0500

    fix: build with correct base path and project images

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/',
-  publicDir: 'portfolio-project-ss',
+  base: '/jahanzaibzafar/',
   build: {
     outDir: 'dist',
   },
